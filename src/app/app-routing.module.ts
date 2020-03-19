c9d7d73dcc9f2c6d41c0365890a991982c9cbd04
MadhushaniHewagama <hewagamamadhushani@gmail.com>
2020-03-19 10:20:17 +0530

    pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignInPageComponent } from './pages/user/sign-in-page/sign-in-page.component';
+import { SignUpPageComponent } from './pages/user/sign-up-page/sign-up-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo:'signIn',
+    pathMatch: 'full'
+  },
+  {
+    path:'signIn',
+    component:SignInPageComponent
+  },
+  {
+    path:'signUp',
+    component:SignUpPageComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,44 +3,40 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainPageComponent } from './src/pages/main-page/main-page.component';
-import { SignInPageComponent } from './src/pages/user/sign-in-page/sign-in-page.component';
-import { SignUpPageComponent } from './src/pages/user/sign-up-page/sign-up-page.component';
-import { HomePageComponent } from './src/pages/user/home-page/home-page.component';
-import { UserHomePageComponent } from './src/pages/user/user-home-page/user-home-page.component';
-import { ViewBookPageComponent } from './src/pages/user/view-book-page/view-book-page.component';
-import { BooksPageComponent } from './src/pages/user/books-page/books-page.component';
-import { CartPageComponent } from './src/pages/user/cart-page/cart-page.component';
-import { OrderPageComponent } from './src/pages/user/order-page/order-page.component';
-import { ViewOrderPageComponent } from './src/pages/user/view-order-page/view-order-page.component';
-import { AdminHhomePageComponent } from './src/pages/admin/admin-hhome-page/admin-hhome-page.component';
-import { AdminHomePageComponent } from './src/pages/admin/admin-home-page/admin-home-page.component';
-import { AddBookPageComponent } from './src/pages/admin/add-book-page/add-book-page.component';
-import { EditBookPageComponent } from './src/pages/admin/edit-book-page/edit-book-page.component';
-import { ViewOrdersPageComponent } from './src/pages/admin/view-orders-page/view-orders-page.component';
-import { ViewUsersPageComponent } from './src/pages/admin/view-users-page/view-users-page.component';
-import { ViewUserPageComponent } from './src/pages/admin/view-user-page/view-user-page.component';
+import { SignInPageComponent } from 'src/app/pages/user/sign-in-page/sign-in-page.component';
+import { SignUpPageComponent } from 'src/app/pages/user/sign-up-page/sign-up-page.component';
+import { UserHomePageComponent } from 'src/app/pages/user/user-home-page/user-home-page.component';
+import { ViewBookPageComponent } from 'src/app/pages/user/view-book-page/view-book-page.component';
+import { BooksPageComponent } from 'src/app/pages/user/books-page/books-page.component';
+import { CartPageComponent } from 'src/app/pages/user/cart-page/cart-page.component';
+import { OrderPageComponent } from 'src/app/pages/user/order-page/order-page.component';
+import { ViewOrderPageComponent } from 'src/app/pages/admin/view-order-page/view-order-page.component';
+import { AdminHomePageComponent } from 'src/app/pages/admin/admin-home-page/admin-home-page.component';
+import { AddBookPageComponent } from 'src/app/pages/admin/add-book-page/add-book-page.component';
+import { EditBookPageComponent } from 'src/app/pages/admin/edit-book-page/edit-book-page.component';
+import { ViewOrdersPageComponent } from 'src/app/pages/admin/view-orders-page/view-orders-page.component';
+import { ViewUsersPageComponent } from 'src/app/pages/admin/view-users-page/view-users-page.component';
+import { ViewUserPageComponent } from 'src/app/pages/admin/view-user-page/view-user-page.component';
+import { ProfileComponent } from 'src/app/pages/user/profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainPageComponent,
     SignInPageComponent,
     SignUpPageComponent,
-    HomePageComponent,
     UserHomePageComponent,
     ViewBookPageComponent,
     BooksPageComponent,
     CartPageComponent,
     OrderPageComponent,
     ViewOrderPageComponent,
-    AdminHhomePageComponent,
     AdminHomePageComponent,
     AddBookPageComponent,
     EditBookPageComponent,
     ViewOrdersPageComponent,
     ViewUsersPageComponent,
-    ViewUserPageComponent
+    ViewUserPageComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,

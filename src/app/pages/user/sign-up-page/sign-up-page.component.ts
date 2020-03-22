1b447996523c09ed134fbce3d63dc71689bd96fb
MadhushaniHewagama <madhushanihewagama.16@cse.mrt.ac.lk>
2020-03-22 11:24:48 +0530

    sign up

--- src/app/pages/user/sign-up-page/sign-up-page.component.ts
+++ src/app/pages/user/sign-up-page/sign-up-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms'
+import { User } from 'src/app/models/user';
+import { UserService } from 'src/app/service/user.service';
 
 @Component({
   selector: 'app-sign-up-page',
@@ -7,9 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpPageComponent implements OnInit {
 
-  constructor() { }
+  public signUpForm: FormGroup;
+  public user: User = new User();
+  public rePwd: string;
+
+  constructor(private userService: UserService) {
+
+
+  }
 
   ngOnInit(): void {
+    this.createForm();
+
   }
+
+  public createForm(): void {
+    this.signUpForm = new FormGroup({
+      name: new FormControl(this.user.name),
+      pwd: new FormControl(this.user.pwd),
+      email: new FormControl(this.user.email),
+      repwd: new FormControl(this.rePwd)
+    });
+
+  }
+
+  public register(): void {
+    this.userService.addUser(this.user).subscribe(
+      res => {
+        console.log(res);
+      },
+      err => { console.log(err); }
+
+
+    )
+  }
+
 
 }
--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import {Observable } from 'rxjs';
-import{API_URL} from '../config/constants'
-import{HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { API_URL } from '../config/constants'
+import { HttpClient } from '@angular/common/http';
+import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +10,18 @@ import{HttpClient} from '@angular/common/http';
 export class UserService {
 
   constructor(private httpClient: HttpClient) { }
-  public getTest():Observable<any>{
-    const url =`${API_URL}/test`;
+  public getTest(): Observable<any> {
+    const url = `${API_URL}/test`;
     return this.httpClient.get(url);
   }
+
+  public addUser(user: User): Observable<any> {
+    const url = `${API_URL}/user`;
+    return this.httpClient.post(url, user);
+  }
+
+
+
 }
+
+
